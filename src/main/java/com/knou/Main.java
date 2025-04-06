2a617fb48abc662935617a469ae3e8e8e85ce9b7
AntenA <80020067+ycs-202007055@users.noreply.github.com>
2025-04-06 16:00:26 +0900

    기본 세팅 및 예시 작성

--- src/main/java/com/knou/Main.java
+++ src/main/java/com/knou/Main.java
@@ -1,7 +1,18 @@
 package com.knou;
 
+import com.knou.Service.Calculate;
+import com.knou.Util.Setting;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Setting.loadProps();
+        String formula1 = "15+15-3*5"; // 15 15 + 3 5 * - : 15
+        String formula2 = "15+15*5"; // 15 15 5 * + : 90
+        String formula3 = "15*15/3+5"; // 15 15 * 3 / 5 + : 80
+        String formula4 = "1.1*2.2"; // 1.1 2.2 * : 2.42
+        String formula5 = "5.43432*5.43"; // 5.43432 5.43 * : 29.5083576
+        Calculate calculate = new Calculate(formula5);
+        calculate.startOperator();
+
     }
 }
